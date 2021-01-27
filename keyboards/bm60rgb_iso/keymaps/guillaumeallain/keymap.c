ec1dadb79e1974f576cdb3e50cd1cf4c825a80af
GuillaumeAllain <13963457+GuillaumeAllain@users.noreply.github.com>
2021-01-26 23:47:33 -0500

    Test unicode send

--- keyboards/bm60rgb_iso/keymaps/guillaumeallain/keymap.c
+++ keyboards/bm60rgb_iso/keymaps/guillaumeallain/keymap.c
@@ -18,6 +18,14 @@
 uint16_t mod_state;
 uint16_t oneshot_mod_state;
 
+/* enum unicode_names { */
+/*     UBSL, */
+/* }; */
+/*  */
+/* const uint32_t PROGMEM unicode_map[] = { */
+/*     [UBSL]  = 0x005C,  // ‽ */
+/* }; */
+
 enum custom_keycodes {
     CM_SCOL = SAFE_RANGE,
     CH_WIND
@@ -46,22 +54,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                      static bool sslkey_registered;
                      if (record->event.pressed) {
                          if (mod_state & MOD_MASK_CTRL) {
-                             // In case only one shift is held
-                             // see https://stackoverflow.com/questions/1596668/logical-xor-operator-in-c
-                             // This also means that in case of holding both shifts and pressing KC_BSPC,
-                             // Shift+Delete is sent (useful in Firefox) since the shift modifiers aren't deleted.
                              del_mods(MOD_MASK_CTRL);
-                             register_code16(KC_COLN);
+                             /* send_unicode_string(":"); */
                              sclkey_registered = true;
                              set_mods(mod_state);
                              return false;
                          } else if (mod_state & MOD_MASK_ALT) {
-                             // In case only one shift is held
-                             // see https://stackoverflow.com/questions/1596668/logical-xor-operator-in-c
-                             // This also means that in case of holding both shifts and pressing KC_BSPC,
-                             // Shift+Delete is sent (useful in Firefox) since the shift modifiers aren't deleted.
                              del_mods(MOD_MASK_ALT);
-                             register_code16(LSFT(KC_NUBS));
+                             /* register_code16(X(UBSL)); */
+                             send_unicode_hex_string("005C");
                              sslkey_registered = true;
                              set_mods(mod_state);
                              return false;
@@ -70,11 +71,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                          }
                      } else {
                          if (sclkey_registered) {
-                             unregister_code16(KC_COLN);
                              sclkey_registered = false;
                              return false;
                          } else if (sslkey_registered){
-                             unregister_code16(LSFT(KC_NUBS));
+                             /* unregister_code16(X(UBSL)); */
                              sslkey_registered = false;
                              return false;
                          } else {
@@ -100,11 +100,11 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_60_iso_arrow(
-        LT(2,KC_NUBS),   KC_1,           KC_2,    KC_3,          KC_4,          KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,         KC_EQL,  KC_BSPC,
-        LALT_T(KC_TAB),  KC_Q,           KC_W,    KC_E,          KC_R,          KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,         KC_RBRC,
-        LCTL_T(KC_ESC),  KC_A,           KC_S,    LT(1, KC_D),   LT(2,KC_F),    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    CM_SCOL, KC_QUOT,         KC_NUHS, KC_SFTENT,
-        KC_LSFT,         KC_GRV,         KC_Z,    KC_X,          KC_C,          KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,         KC_UP,  KC_RSFT,
-        CH_WIND,         MO(1),          CH_WIND,                               LGUI_T(KC_SPC),                     MO(1),   _______, KC_RGUI,         KC_DOWN, KC_RGHT
+        LT(2,KC_NUBS),   KC_1,            KC_2,    KC_3,          KC_4,          KC_5,    KC_6,          KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,           KC_EQL,  KC_BSPC,
+        LALT_T(KC_TAB),  KC_Q,            KC_W,    KC_E,          KC_R,          KC_T,    KC_Y,          KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,           KC_RBRC,
+        LCTL_T(KC_ESC),  KC_A,            KC_S,    LT(1, KC_D),   LT(2,KC_F),    KC_G,    KC_H,          KC_J,    KC_K,    KC_L,    CM_SCOL, KC_QUOT,           KC_NUHS, KC_SFTENT,
+        KC_LSFT,         KC_GRV,          KC_Z,    KC_X,          KC_C,          KC_V,    LT(3,KC_B),    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_UP,   KC_RSFT,
+        LGUI(LSFT(KC_A)),LGUI(LCTL(KC_Q)),CH_WIND,                                        LGUI_T(KC_SPC),                  MO(1),   KC_RGUI, LGUI(LSFT(KC_4)),  KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT_60_iso_arrow(
         _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RESET,
@@ -114,10 +114,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                   _______,  _______, KC_HOME, KC_PGDN, KC_END
     ),
     [2] = LAYOUT_60_iso_arrow(
-        _______, _______, _______, _______, _______, _______, _______,       KC_MPRV,        KC_MPLY,       KC_MNXT,        KC_MUTE, KC_VOLD, KC_VOLU, KC_SLEP,
-        _______, _______, _______, _______, _______, _______, _______,       _______,        _______,       _______,        _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,       _______,        _______,       _______,        KC_MUTE, KC_VOLD, KC_VOLU, KC_SLEP,
+        _______, _______, _______, _______, _______, _______, _______,       _______,        _______,       _______,        KC_MPRV, KC_MPLY, KC_MNXT,
         _______, _______, _______, _______, _______, _______, LCTL(KC_LEFT), LCTL(KC_DOWN),  LCTL(KC_UP),   LCTL(KC_RIGHT), _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,       _______,        _______,       _______,        _______, _______, _______, _______,
         _______, _______, _______,                            LGUI(KC_SPC),                                 _______,        _______, _______, _______, _______
+    ),
+    [3] = LAYOUT_60_iso_arrow(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______
+    ),
+    [4] = LAYOUT_60_iso_arrow(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______
     ),
 };

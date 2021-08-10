ec2cf37c3e8129a33e316d0ae7266d7e81cdaae7
GuillaumeAllain <13963457+GuillaumeAllain@users.noreply.github.com>
2021-08-10 12:11:41 -0400

    small modifications for reset and ergonomy

--- keyboards/bm60rgb_iso/keymaps/splitlayout/keymap.c
+++ keyboards/bm60rgb_iso/keymaps/splitlayout/keymap.c
@@ -34,7 +34,8 @@ enum custom_keycodes {
     KC_EHAT,
     KC_OHAT,
     KC_HCTL,
-    LCTLESC
+    LCTLESC,
+    KC_BTIC
 };
 
 // Tap Dance keycodes
@@ -191,6 +192,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         } else {
             unregister_code(KC_LGUI);
             layer_off(1);
+        }
+        break;
+    case KC_BTIC:
+        if (record->event.pressed){
+            register_code16(LALT(KC_NUHS));
+        } else {
+            unregister_code16(LALT(KC_NUHS));
         }
         break;
 
@@ -352,7 +360,7 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case TD(LSFT_OSL3):
-            return 100;
+            return 120;
         case TD(KC_FWIN):
             return 100;
         case LALT_T(KC_TAB):
@@ -380,14 +388,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(2,KC_NUBS),   KC_1,   KC_2,      KC_3,        KC_4,       KC_5,    KC_6,    KC_7,   KC_8,         KC_9,      KC_0,      KC_MINS,          KC_EQL,      KC_BSPC,
         LALT_T(KC_TAB),  KC_Q,   KC_W,      KC_E,        KC_R,       KC_T,    KC_NO,   KC_NO,  KC_Y,         KC_U,      KC_I,      KC_O,             RCTL_T(KC_P),    
         LCTLESC,         KC_A,   LT(4,KC_S),LT(1, KC_D), LT(2,KC_F), KC_G,    KC_NO,   KC_NO,  KC_HCTL,      LT(1,KC_J),LT(4,KC_K),KC_L,             CM_SCOL,     KC_SFTENT,
-        TD(LSFT_OSL3),   OSL(3), KC_Z,      KC_X,        KC_C,       KC_V,    KC_B,    KC_NO,  LT(3,KC_COMM),KC_N,      KC_M,      KC_DOT,           KC_SLSH,      KC_RSFT,
+        TD(LSFT_OSL3),   OSL(3), KC_Z,      KC_X,        KC_C,       KC_V,    KC_B,    KC_NO,  LT(3,KC_COMM),KC_N,      KC_M,      KC_DOT,           KC_LCTL,      KC_RSFT,
         LGUI(LSFT(KC_A)),LGUI(LCTL(KC_Q)),  CH_WIND,                          LGUI_T(KC_SPC),                MO(1),     KC_RGUI,   LGUI(LSFT(KC_4)), KC_DOWN,     KC_RGHT
     ),
     [1] = LAYOUT_60_iso_arrow(
         KC_NO,   KC_F1,         KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RESET,
         KC_LALT, KC_NO,         KC_1,    KC_2,    KC_3,    KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_GRV,  KC_NO,   KC_NO,   KC_NO,  
         KC_LCTL, KC_0,          KC_4,    KC_5,    KC_6,    KC_NO,         KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,  
-        KC_LSFT, KC_NO,         KC_NO,   KC_7,    KC_8,    KC_9,          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_NO,  
+        KC_LSFT, KC_NO,         KC_NO,   KC_7,    KC_8,    KC_9,          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET, KC_NO,  
         KC_NO,   KC_NO,         KC_NO,                                    LGUI_T(KC_0),              KC_NO,   KC_NO,   KC_HOME, KC_PGDN, KC_END
     ),
     [2] = LAYOUT_60_iso_arrow(
@@ -401,14 +409,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_NO,         KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  
         KC_NO,   KC_NO,         KC_NO,        A(KC_7), A(KC_8), KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_GRV,  KC_NO,   KC_LBRC, KC_NO,   
         KC_NO,   LALT(KC_COMM), LALT(KC_DOT), KC_LPRN, KC_RPRN, KC_NO,   KC_NO,   KC_NO,   KC_SLSH, KC_QUOT, KC_NUHS, KC_RBRC, KC_NO,   KC_NO,  
-        KC_LSFT, KC_NO,         KC_NO,        KC_RBRC, A(KC_9), A(KC_0), KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  
+        KC_LSFT, KC_NO,         KC_NO,        KC_RBRC, A(KC_9), A(KC_0), KC_NO,   KC_NO, S(KC_COMM),KC_NO,   KC_NO,  S(KC_DOT),KC_NO,   KC_NO,  
         KC_NO,   KC_NO,         KC_NO,                                   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______
     ),
     [4] = LAYOUT_60_iso_arrow(
         KC_NO,   KC_NO,      KC_NO,      KC_NO,      KC_NO,         KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,  KC_NO,   KC_NO,   KC_NO,  
         KC_NO,   LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4),    LSFT(KC_EQL),KC_NO,   KC_NO,   KC_NO,  LSFT(KC_MINS),KC_MINS,LSFT(KC_EQL), KC_EQL,  
-        KC_NO,   KC_CIRC,    LALT(KC_MINS), KC_NUBS, LALT(KC_RBRC), KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,  KC_NO,   KC_NO,   KC_NO,  
+        KC_NO,   KC_BTIC,    LALT(KC_MINS), KC_NUBS, LALT(KC_RBRC), KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,  KC_NO,   KC_NO,   KC_NO,  
         KC_NO,   KC_NO,      LSFT(KC_5), LSFT(KC_6), LSFT(KC_7),    LSFT(KC_8),  KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,  KC_NO,   KC_NO,   KC_NO,  
         KC_NO,   KC_NO,      KC_NO,                                              KC_NO,                     KC_NO,       KC_NO,  KC_NO,   KC_NO,   _______
     ),
 };
+
